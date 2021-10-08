4bc762d067d12f72eb548882019eec5f19fd2f1f
Dhrubaraj Roy <dhrubarajroy123@gmail.com>
2021-10-08 20:50:54 +0530

    Topic - Mathematical Expression, Mathematical Function, String Function

--- test.js
+++ test.js
@@ -7,12 +7,43 @@ var b = "This is my line"
 
 
 // to use this variable
-document.write(a);
+document.write(a, "<br>");
 console.log("This is console");         // console use for debbuging perpouses
 
 
-var age = prompt("Tell me your age??");
+//var age = prompt("Tell me your age??");
 
 
 //console.log(age);
-alert(age);
+//alert(age);
+
+//Mathematical expression 
+/*
+document.write("3+4 = ", 3+4, "<br>")
+document.write("3-4 = ", 3-4, "<br>")
+document.write("3*4 = ", 3*4, "<br>")
+document.write("3/4 = ", 3/4, "<br>")
+document.write("3%4 = ", 3%4, "<br>")
+
+
+var chocolateCost  =prompt("What is the total amount::  ")
+var numberOfPeople = prompt("Number of people: ")
+document.write("Total cost of the chocolate is : ", chocolateCost,"<br>");
+document.write("Each of person pay for this is : ", (chocolateCost/numberOfPeople).toFixed(2), "<br>");
+
+*/
+//Mathematical function
+/*
+document.write("2^4 = ", Math.pow(2,4), "<br>");
+document.write("sqrt(36) = ", Math.sqrt(36), "<br>");
+*/
+
+//String Function
+/*
+var str1 = "DhrubarajRoy-GokulNager";
+document.write(str1.length,"<br>");
+document.write(str1.indexOf("Roy"), "<br>");
+document.write(str1.substr(2,6), "<br>");
+document.write(str1.replace("Siliguri", "GokulNager"), "<br>");
+document.write(str1.toUpperCase(), "<br>");
+*/
