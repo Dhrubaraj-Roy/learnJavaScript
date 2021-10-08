fa4de87bbc493c040f8afb93376209d447b93be1
Dhrubaraj Roy <dhrubarajroy123@gmail.com>
2021-10-08 12:38:37 +0530

    learn - what is variable, console, alert

--- test.js
+++ test.js
@@ -1,2 +1,18 @@
-document.write("Another file");
+document.write("Another file", "<br>");
 document.getElementById("th").innerHTML="This is a paragraph";
+
+//variable in javaScript
+var a = 12;
+var b = "This is my line"
+
+
+// to use this variable
+document.write(a);
+console.log("This is console");         // console use for debbuging perpouses
+
+
+var age = prompt("Tell me your age??");
+
+
+//console.log(age);
+alert(age);
